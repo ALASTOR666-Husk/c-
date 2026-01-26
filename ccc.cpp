ddc5f460bb93b5a5f7b047eba788f5269f005d5b
ALASTOR <yaroslav.tolstukhin@mail.ru>
2026-01-26 09:13:08 +0000

    сухов

---
+++ ccc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main ( int argc, char* argv[] ){
+
+    int a;
+    std:: cin >> a;
+    
+    std:: cout << a << std::endl;
+    std:: cout << "HelloWorld" << std::endl;
+    std::cerr << "ERROR" << std:: endl;
+    int nail[9];
+
+    for (int i=0; i < 9; i++){
+        nail[i] = i;
+    }
+    for (int i=0; i < 9; i++){
+        std:: cout << " nail [ " << i << " ] =  " << nail[i] << "\n";
+    }
+    for (int i=0; i < argc; i++){
+        std:: cout <<  " argv [ " << i << " ] =  " << argv[i] << "\n";
+    }
+    return a;
+}
