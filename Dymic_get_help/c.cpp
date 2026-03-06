40747f438e4d181ad4dca28e6c027fe6269a9d5e
ALASTOR <yaroslav.tolstukhin@mail.ru>
2026-03-06 14:19:46 +0000

    saha

---
+++ Dymic_get_help/c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "stone.h"
+using namespace std;
+
+int main() {
+
+    get_help();
+    
+return 0;
+}
+
+
---
+++ Dymic_get_help/stone.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define Author "ABC"
+void get_help() {
+    cout << "Roo" << Author << endl;
+}
